eb807ac7d889243b8cd48c62d0ef3dc729deaa35
anish <jbportals.ismart@gmail.com>
2023-05-04 15:06:55 +0530

    next install package

--- .next/static/webpack/webpack.946fb641cc4a829b.hot-update.js
+++ .next/static/webpack/webpack.946fb641cc4a829b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "496d873cafef1d5c"; }
+/******/ 	__webpack_require__.h = function() { return "3942740100595f93"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
