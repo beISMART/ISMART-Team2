b8cf20a98280dc5e515e1ac164b093c1d8617811
anish <jbportals.ismart@gmail.com>
2023-05-04 18:03:11 +0530

    modified Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ export default function Main() {
       <h2 className="text-2xl font-bold mb-4">Latest Post</h2>
       <div className="bg-white rounded-lg shadow-lg p-4">
         <h3 className="text-lg font-bold mb-2">My First Post</h3>
-        <p className="text-gray-600">Thos is the latest post  from JBPORTALS!. </p>
+        <p className="text-gray-600">This is the latest post  from JB PORTALS!. </p>
         <a href="#" className="text-blue-600 font-semibold mt-2">Read more &rarr;</a>
       </div>
     </div>
