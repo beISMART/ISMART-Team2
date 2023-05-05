28215fd0172c8de68a258c9dd2105e70d92ab2d7
anish <jbportals.ismart@gmail.com>
2023-05-05 15:46:38 +0530

    added dropdown table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import  Header  from '@/components/Header'
 import Main from '@/components/Main'
+import Dropdown from '@/components/dropdown'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
       style={{ backgroundColor: 'skyblue' }}
     >
-     
+     <Dropdown/>
       <Header/>
       <Main/>
     </main>
