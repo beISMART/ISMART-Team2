25e6cbfb8cc7e80b60f020171c1e86c3287ef92e
anish <jbportals.ismart@gmail.com>
2023-05-05 22:58:04 +0530

    final mod for seed and schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,85 +1,157 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import {  PrismaClient } from '@prisma/client';
 import { faker } from "@faker-js/faker";
-
-
 
 const prisma = new PrismaClient();
 
-const getSemesters = (): Prisma.semestersCreateInput[] => [
-  {
-      sem_no: "1",
-  },
-  {
-      sem_no: "2",
-  },
-  {
-      sem_no: "3",
-  },
-  {
-      sem_no: "4",
-  },
-  {
-      sem_no: "5",
-  },
-  {
-      sem_no: "6",
+
+
+
+async function main() {
+  await prisma.student.deleteMany();
+  await prisma.semesters.deleteMany();
+  await prisma.co_admin.deleteMany();
+  await prisma.admin.deleteMany();
+  await prisma.staff.deleteMany();
+  await prisma.academic_year.deleteMany();
+  await prisma.branch.deleteMany();
+  
+
+
+
+
+
+  for (let i = 0; i < 4; i++) {
+    await prisma.branch.createMany({
+      data: {
+        branch_name:faker.commerce.department() // computer science, mechanical, EEE, civil
+      },
+    });
   }
-];
+
+
+  for (let i = 0; i < 4; i++) {
+    await prisma.academic_year.create({
+      data: {
+        Academic_year:faker.date.between('2020-01-01', '2023-01-01'),
+
+      },
+    });
+  }
+
+
+
+const branches = await prisma.branch.findMany();
+const academic_years = await prisma.academic_year.findMany();
+const randomIndex = Math.floor(Math.random() * academic_years.length);
+await Promise.all(branches.map(async (branch)=> {
+  await prisma.staff.create({
+    data:{
+      first_name:faker.name.firstName(),
+      last_name:faker.name.lastName(),
+      full_name:faker.name.fullName(),
+      contact_number:faker.phone.number("+91 ##### #####"),
+      gender:faker.name.sex(),
+      mail_id:faker.internet.email(),
+      address:faker.address.streetAddress(),
+      city:faker.address.cityName(),
+      state:faker.address.state(),
+      staff_qualification:faker.lorem.sentence(),
+      branch_name:branch?.branch_name!,
+      Academic_year: academic_years[randomIndex].Academic_year,
+        
+      
+    }
+  })
+}))
+    
+for (let i = 0; i < 10; i++) {
+ await prisma.admin.createMany({
+  data:{
+    first_name:faker.name.firstName(),
+    last_name:faker.name.lastName(),
+    full_name:faker.name.fullName(),
+    contact_number:faker.phone.number("+91 ##### #####"),
+    gender:faker.name.gender(),
+    mail_id:faker.internet.email(),
+    address:faker.address.streetAddress(),
+    city:faker.address.cityName(),
+    state:faker.address.state(),
+    admin_qualification:faker.lorem.sentence(),
+   },
+  
+})
+}
+
+for (let i = 0; i < 2; i++) {
+const staffs = await prisma.staff.findMany();
+await Promise.all(staffs.map(async (staff)=> {
+  await prisma.co_admin.create({
+    data:{
+     
+        staffId:staff.id,
+      
+    }
+  })
+}))
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const main = async () => {
-  await PrismaClient.semesters.deleteMany();
-  await client.session_details.deleteMany();
-  await client.test_history.deleteMany();
-  await client.student_test_details.deleteMany();
-  await client.questions_details.deleteMany();
-  await client.test_details.deleteMany();
-  await client.review_reply.deleteMany();
-  await client.review.deleteMany();
-  await client.favourites.deleteMany();
-  await client.notes.deleteMany();
-  await client.user_details.deleteMany();
-  await client.users.deleteMany();
-  await client.subjects.deleteMany();
-  await client.semesters.deleteMany();
-  await client.branch.deleteMany();
-  await client.college.deleteMany();
-
- 
-  await PrismaClient.semesters.createMany({
-      data: getSemesters(),
+for (let i = 0; i < 2; i++) {
+  await prisma.semesters.create({
+    data: {
+      sem_no: faker.datatype.number({ min:1 , max:6 }).toString()
+    }  
   });
+}
+
+
+
+
+
+
+
+for (let i = 0; i < 5; i++) {
+const Semesters = await prisma.semesters.findMany();
+const randomIndex1 = Math.floor(Math.random() * Semesters.length);
+await Promise.all(branches.map(async (branch)=> {
+  await prisma.student.create({
+    data:{
+      first_name:faker.name.firstName(),
+      last_name:faker.name.lastName(),
+      full_name:faker.name.fullName(),
+      contact_number:faker.phone.number("+91 ##### #####"),
+      gender:faker.name.sex(),
+      mail_id:faker.internet.email(),
+      address:faker.address.streetAddress(),
+      city:faker.address.cityName(),
+      state:faker.address.state(),
+      reg_no:faker.random.alphaNumeric(),
+      Branch_name:branch?.branch_name!,
+      Academic_year: academic_years[randomIndex].Academic_year,
+      Sem_no:Semesters[randomIndex1].sem_no
+    },
+  
+  })
+}))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -87,7 +159,6 @@ const main = async () => {
 }
 
 main()
-  .catch((e) => console.error(e))
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+  .then(() => console.log("success"))
+  .catch((e) => console.log(e))
+  .finally(async () => await prisma.$disconnect());
